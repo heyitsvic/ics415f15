a42593b7926bd1e88acbd40e14c399e359ee7608
heyitsvic <victorsi@hawaii.edu>
2015-09-15 11:41:36 -1000

    Finish w4c1
    Made mouse events work on 'this' element.

---
+++ IC/w4c1/clicks.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Victor on 9/15/2015.
+ */
+function changeColor(elem, color) {
+  elem.style.color = color;
+}
+
+function changeShadow(elem, color) {
+  elem.style.color = color;
+}
